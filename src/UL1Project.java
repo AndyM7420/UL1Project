ad69b3527bf76f6740d49dbe3e53dcc140fcb66f
Sampleproject <amejia9149@bths.edu>
2022-09-28 16:50:32 -0400

    Initial commit

---
+++ src/UL1Project.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class UL1Project  {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        double noTaxBill;
+        double tipPercentage;
+        double tipAmount;
+        double totalTipAmount;
+        int partyAmount;
+        double totalBill;
+        double personTip;
+        double  eachPersonAmount;
+        System.out.println("");
+        System.out.println("Hello User! Please input the bill amount, tip percentage as a whole number, and the number of people in the party ");
+        noTaxBill = s.nextDouble();
+        tipPercentage = s.nextDouble();
+        partyAmount = s.nextInt();
+        tipAmount = tipPercentage/100.0;
+        totalTipAmount = (noTaxBill*tipAmount);
+        totalTipAmount = (double) Math.round(totalTipAmount*100)/100.0;
+        totalBill = totalTipAmount + noTaxBill;
+        totalBill = Math.round(totalBill*100)/100.0;
+        personTip = totalTipAmount/partyAmount;
+        personTip = Math.round(personTip*100)/100.00;
+        totalTipAmount = (double) Math.round(totalTipAmount*100)/100.0;
+        eachPersonAmount = totalBill/partyAmount;
+        eachPersonAmount = Math.round(eachPersonAmount*100)/100.00;
+        System.out.print("Thank you. Your total tip amount is  $" + totalTipAmount + " Your total bill with tip is:  $" + totalBill + " Each person must pay  $" + personTip+ " each for tip. The total per person is: $" + eachPersonAmount);
+
+    }
+}
